e4423f077350cfb99c6b90e0a957adf0fae77f05
KauaO <kaua.o@escolar.ifrn.edu.br>
2022-12-15 11:43:32 -0300

    Cadastrar_Produto-JqueryOK

---
+++ oficina/static/ocultar.js
@@ -0,0 +1,71 @@
+$("#select").change(function(){
+    var value = $('#select option:selected').text();
+    var largura = document.getElementById('largura');
+    var perfil = document.getElementById('perfil');
+    var tamanho = document.getElementById('tamanho');
+    var marca = document.getElementById('marca');
+    var tipo = document.getElementById('tipo');
+    var material = document.getElementById('material');
+    var cor = document.getElementById('cor');
+    var btn = document.getElementById('btn');
+    var quant = document.getElementById('quant');
+    var valor = document.getElementById('val');
+    var img = document.getElementById('img');
+
+        if(value === 'pneu'){
+            $(largura).show();
+            $(perfil).show();
+            $(tamanho).show();
+            $(btn).show();
+            $(quant).show();
+            $(val).show();
+            $(img).show();
+        }
+
+        if(value === 'aro'){
+            $(largura).show();
+            $(perfil).show();
+            $(tamanho).show();
+            $(material).show();
+            $(cor).show();
+            $(btn).show();
+            $(quant).show();
+            $(val).show();
+            $(img).show();
+        }
+
+        if(value === 'raio'){
+            $(largura).show();
+            $(perfil).show();
+            $(tamanho).show();
+            $(tipo).show();
+            $(material).show();
+            $(cor).show();
+            $(btn).show();
+            $(quant).show();
+            $(val).show();
+            $(img).show();
+        }
+
+        if(value === 'farol'){
+            
+            $(tamanho).show();
+            $(tipo).show();
+            $(material).hide();
+            $(btn).show();
+            $(quant).show();
+            $(val).show();
+            $(img).show();
+        }
+
+        if(value === 'retrovisor'){
+            $(tamanho).show();
+            $(tipo).show();
+            $(material).show();
+            $(cor).show();
+            $(btn).show();
+            $(quant).show();
+            $(val).show();
+            $(img).show();
+        }
+});
