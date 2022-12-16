879c3c5d8c64e648980a345edc684e9dc896a759
KauaO <kaua.o@escolar.ifrn.edu.br>
2022-12-16 08:11:22 -0300

    Deu certo

--- oficina/static/js/form.js
+++ oficina/static/js/form.js
@@ -1,56 +1,55 @@
-const form = document.getElementById('form');
-const largura = document.getElementById('txtlargura');
-const perfil = document.getElementById('txtperfil');
-const tamanho = document.getElementById('txttamanho');
-const marca = document.getElementById('txtmarca');
-const tipo = document.getElementById('txttipo');
-const material = document.getElementById('txtmaterial');
-const cor = document.getElementById('txtcor');
-const valor = document.getElementById('txtvalor');
-const quantidade = document.getElementById('txtquantidade');
+const form = document.getElementById("form");
+const largura = document.getElementById("txtlargura");
+const perfil = document.getElementById("txtperfil");
+const tamanho = document.getElementById("txttamanho");
+const marca = document.getElementById("txtmarca");
+const tipo = document.getElementById("txttipo");
+const material = document.getElementById("txtmaterial");
+const cor = document.getElementById("txtcor");
+const valor = document.getElementById("txtvalor");
+const quantidade = document.getElementById("txtquantidade");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    checkInputs();
+  checkInputs();
 });
 
 function checkInputs() {
-    const larguraValue = largura.value;
-    const perfilValue = perfil.value;
-    const tamanhoValue = tamanho.value;
-    const marcaValue = marca.value;
-    const tipoValue = tipo.value;
-    const materialValue = material.value;
-    const corValue = cor.value;
-    const valorValue = valor.value;
-    const quantidadeValue = quantidade.value;
+  const larguraValue = largura.value;
+  const perfilValue = perfil.value;
+  const tamanhoValue = tamanho.value;
+  const marcaValue = marca.value;
+  const tipoValue = tipo.value;
+  const materialValue = material.value;
+  const corValue = cor.value;
+  const valorValue = valor.value;
+  const quantidadeValue = quantidade.value;
 
-    if (valorValue == "") {
-        setErrorFor(valor, "O campo valor é obrigatório.");
-        console.log("----aqui")
-    } else {
-        setSuccessFor(valor);
-    }
-    if (perfilValue == "") {
-        setErrorFor(perfil, "O campo perfil é obrigatório.");
-    } else {
-        setSuccessFor(perfil);
-    }
+  if (valorValue == "") {
+    setErrorFor(valor, "O campo valor é obrigatório.");
+    console.log("----aqui");
+  } else {
+    setSuccessFor(valor);
+  }
+  if (perfilValue == "") {
+    setErrorFor(perfil, "O campo perfil é obrigatório.");
+  } else {
+    setSuccessFor(perfil);
+  }
 }
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
 
-    small.innerText = message; //adiciona mensagem de erro
+  small.innerText = message; //adiciona mensagem de erro
 
-    formControl.className = 'form-control error';
+  formControl.className = "form-control error";
 }
 
 function setSuccessFor(input) {
-    const formControl = input.parentElement; //retorna a div do input
+  const formControl = input.parentElement; //retorna a div do input
 
-    formControl.className = "form-control success";
-
+  formControl.className = "form-control success";
 }
--- oficina/static/js/ocultar.js
+++ oficina/static/js/ocultar.js
@@ -1,83 +1,82 @@
-$("#select").change(function(){
-    var value = $('#select option:selected').text();
-    var largura = document.getElementById('largura');
-    var perfil = document.getElementById('perfil');
-    var tamanho = document.getElementById('tamanho');
-    var marca = document.getElementById('marca');
-    var tipo = document.getElementById('tipo');
-    var material = document.getElementById('material');
-    var cor = document.getElementById('cor');
-    var btn = document.getElementById('btn');
-    var quant = document.getElementById('quant');
-    var valor = document.getElementById('val');
-    var img = document.getElementById('img');
+$("#select").change(function () {
+  var value = $("#select option:selected").text();
+  var largura = document.getElementById("largura");
+  var perfil = document.getElementById("perfil");
+  var tamanho = document.getElementById("tamanho");
+  var marca = document.getElementById("marca");
+  var tipo = document.getElementById("tipo");
+  var material = document.getElementById("material");
+  var cor = document.getElementById("cor");
+  var btn = document.getElementById("btn");
+  var quant = document.getElementById("quant");
+  var valor = document.getElementById("val");
+  var img = document.getElementById("img");
 
-    console.log(value);
-    $(largura).hide();
-    $(tipo).hide();
-    $(perfil).hide();
-    $(tamanho).hide();
+  console.log(value);
+  $(largura).hide();
+  $(tipo).hide();
+  $(perfil).hide();
+  $(tamanho).hide();
+  $(material).hide();
+  $(cor).hide();
+  $(btn).hide();
+  $(quant).hide();
+  $(val).hide();
+  $(img).hide();
+
+  if (value === "pneu") {
+    console.log("Mizera dos inferno");
+    $(largura).show();
+    $(perfil).show();
+    $(tamanho).show();
+    $(btn).show();
+    $(quant).show();
+    $(val).show();
+    $(img).show();
+  }
+
+  if (value === "aro") {
+    $(largura).show();
+    $(perfil).show();
+    $(tamanho).show();
+    $(material).show();
+    $(cor).show();
+    $(btn).show();
+    $(quant).show();
+    $(val).show();
+    $(img).show();
+  }
+
+  if (value === "raio") {
+    $(largura).show();
+    $(perfil).show();
+    $(tamanho).show();
+    $(tipo).show();
+    $(material).show();
+    $(cor).show();
+    $(btn).show();
+    $(quant).show();
+    $(val).show();
+    $(img).show();
+  }
+
+  if (value === "farol") {
+    $(tamanho).show();
+    $(tipo).show();
     $(material).hide();
-    $(cor).hide();
-    $(btn).hide();
-    $(quant).hide();
-    $(val).hide();
-    $(img).hide();
-
-        if(value === 'pneu'){
-            console.log("Mizera dos inferno");
-            $(largura).show();
-            $(perfil).show();
-            $(tamanho).show();
-            $(btn).show();
-            $(quant).show();
-            $(val).show();
-            $(img).show();
-        }
-
-        if(value === 'aro'){
-            $(largura).show();
-            $(perfil).show();
-            $(tamanho).show();
-            $(material).show();
-            $(cor).show();
-            $(btn).show();
-            $(quant).show();
-            $(val).show();
-            $(img).show();
-        }
-
-        if(value === 'raio'){
-            $(largura).show();
-            $(perfil).show();
-            $(tamanho).show();
-            $(tipo).show();
-            $(material).show();
-            $(cor).show();
-            $(btn).show();
-            $(quant).show();
-            $(val).show();
-            $(img).show();
-        }
-
-        if(value === 'farol'){
-            
-            $(tamanho).show();
-            $(tipo).show();
-            $(material).hide();
-            $(btn).show();
-            $(quant).show();
-            $(val).show();
-            $(img).show();
-        }
-        if(value === 'retrovisor'){
-            $(tamanho).show();
-            $(tipo).show();
-            $(material).show();
-            $(cor).show();
-            $(btn).show();
-            $(quant).show();
-            $(val).show();
-            $(img).show();
-        }
+    $(btn).show();
+    $(quant).show();
+    $(val).show();
+    $(img).show();
+  }
+  if (value === "retrovisor") {
+    $(tamanho).show();
+    $(tipo).show();
+    $(material).show();
+    $(cor).show();
+    $(btn).show();
+    $(quant).show();
+    $(val).show();
+    $(img).show();
+  }
 });
